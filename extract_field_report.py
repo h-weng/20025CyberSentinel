5b2818d3992829bec23c02027192f62905c44401
hue <hweng@acme-nyc.com>
2025-07-04 22:35:32 +0000

    Update extract_field_report.py

--- extract_field_report.py
+++ extract_field_report.py
@@ -24,7 +24,7 @@ import re
 
 def altgen(n, t):
     """
-    generator for aternating numbers: [1234, 1233, 1235, 1233, 1236 ...]
+    generator for aternating numbers: [1234, 1233, 1235, 1232, 1236 ...]
     
     """
     a=0
